5d611966a2c920bc100cf14da01b54ac4dfd5c86
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-03-01 21:26:34 +0900

    12~16 commit / lecture finished

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 var fs = require("fs");
-var path = require("path");
-var qs = require("querystring");
 var bodyParser = require("body-parser");
 var compression = require("compression");
-var sanitizeHtml = require("sanitize-html");
-var template = require("./lib/template.js");
+var helmet = require("helmet");
 
+var indexRouter = require("./routes/index");
+var topicRouter = require("./routes/topic");
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.use(helmet());
+
 app.get("*", function (req, res, next) {
   fs.readdir("./data", function (error, filelist) {
     req.list = filelist;
@@ -17,184 +20,16 @@ app.get("*", function (req, res, next) {
   });
 });
 
-app.get(
-  "/hee/:id",
-  function (req, res, next) {
-    if (req.params.id === "0") next("route");
-    else next();
-  },
-  function (req, res, next) {
-    res.send("regular");
-  }
-);
+app.use("/", indexRouter);
+app.use("/topic", topicRouter);
 
-app.get("/hee/:id", function (req, res, next) {
-  res.send("special");
+app.use(function (req, res, next) {
+  res.status(404).send("sorry cant find that!");
 });
 
-app.get("/", (req, res) => {
-  var title = "Welcome";
-  var description = "Hello, Node.js";
-  var list = template.list(req.list);
-  var html = template.HTML(
-    title,
-    list,
-    `<h2>${title}</h2>${description}`,
-    `<a href="/create">create</a>`
-  );
-  res.send(html);
-});
-
-app.get("/page/:pageId", (req, res) => {
-  var filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    var title = req.params.pageId;
-    var sanitizedTitle = sanitizeHtml(title);
-    var sanitizedDescription = sanitizeHtml(description, {
-      allowedTags: ["h1", "br"],
-    });
-    var list = template.list(req.list);
-    var html = template.HTML(
-      sanitizedTitle,
-      list,
-      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      ` <a href="/create">create</a>
-      <a href="/update/${sanitizedTitle}">update</a>
-      <form action="/delete_process" method="post">
-        <input type="hidden" name="id" value="${sanitizedTitle}">
-        <input type="submit" value="delete">
-      </form>`
-    );
-    res.send(html);
-  });
-});
-
-app.get("/create", (req, res) => {
-  var title = "WEB - create";
-  var list = template.list(filelist);
-  var html = template.HTML(
-    title,
-    list,
-    `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `,
-    ""
-  );
-  res.send(html);
-});
-
-app.post("/create_process", (req, res) => {
-  /*
-  var body = "";
-  req.on("data", function (data) {
-    body = body + data;
-  });
-  req.on("end", function () {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/page/${title}`);
-    });
-  });
-  */
-
-  var post = req.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-    res.redirect(`/page/${title}`);
-  });
-});
-
-app.get("/update/:pageId", (req, res) => {
-  var filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    var title = req.params.pageId;
-    var list = template.list(filelist);
-    var html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-    );
-    res.send(html);
-  });
-});
-
-app.post("/update_process", (req, res) => {
-  var post = req.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/page/${title}`);
-    });
-  });
-});
-
-app.post("/delete_process", (req, res) => {
-  var post = req.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    res.redirect(`/`);
-  });
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(3000, () => console.log("Example app liistening on port 3000"));
-
-/* 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-      }
-    } else if(pathname === '/create'){
-      
-    } else if(pathname === '/create_process'){
-      
-    } else if(pathname === '/update'){
-      
-    } else if(pathname === '/update_process'){
-      
-    } else if(pathname === '/delete_process'){
-      
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
-});
-app.listen(3000);
-
-*/
